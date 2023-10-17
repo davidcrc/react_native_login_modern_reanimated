24431f7a47d90cc1b59de0c0886c99936659532e
davidcrc <david_drcr@hotmail.com>
2023-10-17 12:04:51 -0500

    feat: :zap: basic config eslint, prettier

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          // This has to be mirrored in tsconfig.json
+          '^@/(.+)': './src/\\1',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
